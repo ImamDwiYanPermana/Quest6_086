405ff2f709ebd03fd85cadb33227e3b9bf269615
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-22 16:12:52 +0700

    Membuat enum class halaman, membuat fungsi utama berparameter

--- app/src/main/java/com/example/minggu8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/minggu8/navigation/PengelolaHalaman.kt
@@ -1,2 +1,46 @@
 package com.example.minggu8.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.minggu8.ui.screen.SplashView
+import com.example.minggu8.ui.viewModel.MahasiswaViewModel
+import com.example.minggu8.ui.viewModel.RencanaStudyViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ) {
+        composable(route = Halaman.Splash.name) {
+            SplashView(onMulaiButton = {
+                navController.navigate(
+                    Halaman.Mahasiswa.name
+                )
+            })
+        }
+    }
+}
+
+

5140aeb139d3664e57944187286e6aa0cf9a8e5d
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-22 15:49:39 +0700

    masih di tampil kita membuat fungsi cardSection yang berparameter judul param, isiparam dan membuat struktur layout menggunakan column dan row

--- app/src/main/java/com/example/minggu8/ui/screen/Tampil.kt
+++ app/src/main/java/com/example/minggu8/ui/screen/Tampil.kt
@@ -1,19 +1,24 @@
 package com.example.minggu8.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.minggu8.model.Mahasiswa
 import com.example.minggu8.model.RencanaStudy
-import java.lang.reflect.Modifier
+
 
 @Composable
 fun Tampil(
@@ -55,6 +60,23 @@ fun Tampil(
 
         Button(onClick = {onClickButton()}) {
             Text(text = "Halaman Utama")
+        }
+    }
+}
+
+@Composable
+fun CardSection(judulParam: String, isiParam: String) {
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = judulParam, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(
+                text = "$isiParam",
+                modifier = Modifier.weight(2f)
+            )
         }
     }
 }
